7bbf028c2c2a1474ffb2f605746294a1c25fb51f
Franco <francofl1999@gmail.com>
2020-06-23 13:40:41 -0300

    World map - 2

--- world_data/world_population.py
+++ world_data/world_population.py
@@ -1,27 +1,50 @@
 import json
-import pygal.maps.world
-from country_codes import get_country_code
+from pygal.maps.world import World
 
-# Load the data into a list
-filename = 'population_data.json'
-with open(filename) as f:
-    pop_data = json.load(f)
 
-# Build a dictionary of population data
-cc_populations = {}
-for pop_dict in pop_data:
-    if pop_dict['Year'] == '2010':
-        country_name = pop_dict['Country Name']
-        population = int(float(pop_dict['Value']))
-        code = get_country_code(country_name)
-        if code:
-            cc_populations[code] = population
-            
-wm = pygal.maps.world.World()
-wm.title = 'World population in 2010, by Country'
-wm.add('2010', cc_populations)
+def get_country_code(country_name, codes_and_names):
+    """Return the Pygal 2-digit country code for the given country."""
+    
+    for each_code, each_name in codes_and_names.items():
+        if country_name == each_name:
+            return each_code
+       
+    return None
 
-wm.render_to_file("world_population.svg")
+def get_country_list():
+    filename = 'country_list.txt'
+    codes_and_names = {}
+    with open(filename) as f:
+        for line in f.readlines():
+            code = line[0:2]
+            name = line[3:len(line)].strip()
+            codes_and_names[code] = name
 
-print(cc_populations)
-        
+    return codes_and_names
+    
+def make_world_map():
+    # Load the data into a list
+    filename = 'population_data.json'
+    with open(filename) as f:
+        pop_data = json.load(f)
+    # Get the two digits cuntries codes
+    codes_and_names = get_country_list()
+    # Build a dictionary of population data
+    cc_populations = {}
+    for pop_dict in pop_data:
+        if pop_dict['Year'] == '2010':
+            country_name = pop_dict['Country Name']
+            population = int(float(pop_dict['Value']))
+            code = get_country_code(country_name, codes_and_names)
+            if code:
+                cc_populations[code] = population
+    # Make world map        
+    wm = World()
+    wm.title = 'World population in 2010, by Country'
+    wm.add('2010', cc_populations)
+    # Convert it to file 
+    wm.render_to_file("world_population.svg")
+
+make_world_map()
+
+
